ef7518ac8347d9141b248dc67309ddec9d19e473
Radu-Paul PALADE <rpalade@192-168-0-125.rdsnet.ro>
2020-04-02 15:28:30 +0300

    indentation change

--- src/resources/user/user.controllers.js
+++ src/resources/user/user.controllers.js
@@ -4,39 +4,39 @@ import { crudControllers } from '../../utils/crud'
 export const user = crudControllers(User)
 
 export const me = (req, res) => {
-  res.status(200).json({ data: req.user })
+    res.status(200).json({ data: req.user })
 }
 
 export const getAllUsers = model => async (req, res) => {
-  res.status(200).send('talent')
-  try {
-    const docs = await model
-      .find({})
-      .lean()
-      .exec()
+    res.status(200).send('talent')
+    try {
+        const docs = await model
+            .find({})
+            .lean()
+            .exec()
 
-    res.status(200).json({ data: docs })
-  } catch (e) {
-    console.error(e)
-    res.status(400).end()
-  }
+        res.status(200).json({ data: docs })
+    } catch (e) {
+        console.error(e)
+        res.status(400).end()
+    }
 }
 
 export const updateMe = async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(
-      req.body.user._id,
-      req.body.body,
-      {
-        new: true
-      }
-    )
-      .lean()
-      .exec()
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.body.user._id,
+            req.body.body,
+            {
+                new: true
+            }
+        )
+            .lean()
+            .exec()
 
-    res.status(200).json({ data: user })
-  } catch (e) {
-    console.error(e)
-    res.status(400).end()
-  }
+        res.status(200).json({ data: user })
+    } catch (e) {
+        console.error(e)
+        res.status(400).end()
+    }
 }
