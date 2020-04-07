3daa28deb3b71e17b56ad932f5b852fb46fbaff5
Radu-Paul PALADE <rpalade@p5ro-port1024.local>
2020-04-07 16:57:26 +0300

    added price

--- src/resources/product/product.model.js
+++ src/resources/product/product.model.js
@@ -1,37 +1,43 @@
 import mongoose from 'mongoose'
 
 const productSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-      maxlength: 50
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 50
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 100
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            trim: true,
+            maxlength: 3
+        },
+        price: {
+            type: Number,
+            required: true,
+            trim: true,
+            maxlength: 3
+        },
+        createdBy: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'user',
+            required: true
+        }
+        // list: {
+        //   type: mongoose.SchemaTypes.ObjectId,
+        //   ref: 'list',
+        //   required: true
+        // }
     },
-    description: {
-      type: String,
-      required: true,
-      trim: true,
-      maxlength: 100
-    },
-    quantity: {
-      type: Number,
-      required: true,
-      trim: true,
-      maxlength: 3
-    },
-    createdBy: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: 'user',
-      required: true
-    }
-    // list: {
-    //   type: mongoose.SchemaTypes.ObjectId,
-    //   ref: 'list',
-    //   required: true
-    // }
-  },
-  { timestamps: true }
+    { timestamps: true }
 )
 
 productSchema.index({ list: 1, name: 1 }, { unique: true })
