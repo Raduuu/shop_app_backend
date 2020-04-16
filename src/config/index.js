8f49c680011735b49aa870191d130fb79b58b374
Radu-Paul PALADE <rpalade@192-168-0-125.rdsnet.ro>
2020-04-16 16:36:48 +0300

    changed port

--- src/config/index.js
+++ src/config/index.js
@@ -1,30 +1,31 @@
 import { merge } from 'lodash'
 const env = process.env.NODE_ENV || 'development'
+const port = process.env.PORT || 9000
 
 const baseConfig = {
-  env,
-  isDev: env === 'development',
-  isTest: env === 'testing',
-  port: 9000,
-  secrets: {
-    jwt: process.env.JWT_SECRET,
-    jwtExp: '100d'
-  }
+    env,
+    isDev: env === 'development',
+    isTest: env === 'testing',
+    port,
+    secrets: {
+        jwt: process.env.JWT_SECRET,
+        jwtExp: '100d'
+    }
 }
 
 let envConfig = {}
 
 switch (env) {
-  case 'dev':
-  case 'development':
-    envConfig = require('./dev').config
-    break
-  case 'test':
-  case 'testing':
-    envConfig = require('./testing').config
-    break
-  default:
-    envConfig = require('./dev').config
+    case 'dev':
+    case 'development':
+        envConfig = require('./dev').config
+        break
+    case 'test':
+    case 'testing':
+        envConfig = require('./testing').config
+        break
+    default:
+        envConfig = require('./dev').config
 }
 
 export default merge(baseConfig, envConfig)
