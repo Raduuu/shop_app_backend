e742c957e9dfee1262b9bd5b3b185b8768f8b9f2
Radu Palade <p.radupaul@gmail.com>
2023-07-08 13:17:56 +0300

    packages update and small change in db

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
 import options from '../config'
 
-export const connect = (url = options.dbUrl, opts = {}) => {
-  return mongoose.connect(
-    url,
-    { ...opts, useNewUrlParser: true }
-  )
-}
+// eslint-disable-next-line import/prefer-default-export
+export const connect = (url = options.dbUrl, opts = {}) =>
+    mongoose.connect(url, {
+        ...opts,
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
